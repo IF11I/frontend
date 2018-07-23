18aa5825bd1e8ca6eb7d1d94b8810c91d7d740e7
Nils Weber <me@nilsweb.de>
2018-07-23 18:38:14 +0200

    Added toolbar at the top

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import {
+  MatToolbarModule,
   MatSidenavModule,
   MatButtonModule,
 } from '@angular/material';
 
 @NgModule({
   imports: [
+    MatToolbarModule,
     MatSidenavModule,
     MatButtonModule,
   ],
   exports: [
+    MatToolbarModule,
     MatSidenavModule,
     MatButtonModule,
   ],
