47da8941c796d453bc22cd8d093c3c8e850a89d4
Nils Weber <me@nilsweb.de>
2018-07-23 10:28:40 +0200

    Added model interfaces
    The following interfaces have been added:
    - Room
    - Component
    - Attribute
    - Supplier

---
+++ src/app/model/supplier.ts
@@ -0,0 +1,11 @@
+export interface Supplier {
+  id?: number;
+  name: string;
+  street: string;
+  postalCode: string;
+  city: string;
+  telephone: string;
+  mobile: string;
+  fax: string;
+  email: string;
+}
