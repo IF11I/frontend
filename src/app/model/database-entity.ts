fc45c9ec5eace08061dd50f98ac72501a96005e4
Nils Weber <me@nilsweb.de>
2018-07-25 11:18:28 +0200

    Commented model classes

--- src/app/model/database-entity.ts
+++ src/app/model/database-entity.ts
@@ -1,3 +1,11 @@
+/**
+ * A class for every database entity.
+ *
+ * @author Nils Weber
+ */
 export class DatabaseEntity {
+
+  /** @param id The entity's id. */
   constructor(public readonly id?: number) { }
+
 }
--- src/app/model/supplier.ts
+++ src/app/model/supplier.ts
@@ -1,12 +1,34 @@
 import { DatabaseEntity } from './database-entity';
 
+/**
+ * A supplier.
+ *
+ * @author Nils Weber
+ */
 export class Supplier extends DatabaseEntity {
+
+  /** The supplier's name. */
   public name: string;
+
+  /** The supplier's street. */
   public street: string;
+
+  /** The supplier's postal code or zip. */
   public postalCode: string;
+
+  /** The supplier's city. */
   public city: string;
+
+  /** The supplier's telephone number. */
   public telephone: string;
+
+  /** The supplier's mobile phone number. */
   public mobile: string;
+
+  /** The supplier's fax number. */
   public fax: string;
+
+  /** The supplier's email address. */
   public email: string;
+
 }
