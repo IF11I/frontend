849ffacb43e9387ba6438cb8e358b3774129931d
Nils Weber <me@nilsweb.de>
2018-07-25 11:15:39 +0200

    Commented ComponentTypeDetailComponent

--- src/app/components/component-type-detail/component-type-detail.component.ts
+++ src/app/components/component-type-detail/component-type-detail.component.ts
@@ -3,15 +3,20 @@ import { Title } from '@angular/platform-browser';
 import { Router, ActivatedRoute, ParamMap, Params } from '@angular/router';
 import { MatDialog, MatSelectionList } from '@angular/material';
 
-import { Observable, of } from 'rxjs';
+import { of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 
 import { ComponentType } from 'src/app/model/component-type';
 import { Attribute } from 'src/app/model/attribute';
 import { ComponentTypeService } from 'src/app/services/component-type.service';
 import { AttributeService } from 'src/app/services/attribute.service';
-import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
+import { ConfirmationDialogComponent } from 'src/app/components/confirmation-dialog/confirmation-dialog.component';
 
+/**
+ * Component for handling displaying/editing/deleting a single component type.
+ *
+ * @author Nils Weber
+ */
 @Component({
   selector: 'app-component-type-detail',
   templateUrl: './component-type-detail.component.html',
@@ -19,10 +24,16 @@ import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation
 })
 export class ComponentTypeDetailComponent implements OnInit {
 
+  /** Reference to the attribute list. */
   @ViewChild('attributesList') attributesList: MatSelectionList;
 
+
+  /** The component type currently displayed. */
   private componentType = new ComponentType();
+
+  /** The list of all available attributes. */
   private attributes: Attribute[] = [];
+
 
   constructor(
     private title: Title,
@@ -34,55 +45,87 @@ export class ComponentTypeDetailComponent implements OnInit {
   ) { }
 
 
+  /**
+   * Lifecycle-Hook: On Init
+   *
+   * @author Nils Weber
+   */
   ngOnInit() {
+    // Set app title.
     this.title.setTitle('IT-Verwaltung · Component Types · Detail');
 
+    // Get all available attributes.
     this.attributeService.getAttributes().subscribe(attrs => this.attributes = attrs);
 
+    // Get the current component type by the id that was passed in through a route parameter.
     this.route.paramMap.pipe(
       switchMap((params: Params) => {
         const idParam = params.get('id');
+
         if (idParam === 'new') {
+          // Create a new component type.
           this.title.setTitle('IT-Verwaltung · Component Types · New');
           return of(new ComponentType());
         } else {
+          // Get the existing component type.
           return this.componentTypeService.getTypeById(+idParam);
         }
       })
-    ).subscribe(componentType => this.setComponentType(componentType));
+    ).subscribe(componentType => this.componentType = componentType);
   }
 
 
+  /**
+   * Check if the given attribute is part of the component type.
+   *
+   * @param attribute The attribute to check against.
+   *
+   * @author Nils Weber
+   */
   private isAttributeSelected(attribute: Attribute) {
     return !!this.componentType.attributes.find(attr => attr.id === attribute.id);
   }
 
 
-  private setComponentType(componentType: ComponentType) {
-    this.componentType = componentType;
-  }
-
-
+  /**
+   * Save or create the current component type.
+   *
+   * @author Nils Weber
+   */
   private saveComponentType() {
+    // Get the selected attributes and add them to the component type.
     const selectedAttributes = this.attributesList.selectedOptions.selected.map(item => item.value);
     this.componentType.attributes = selectedAttributes;
 
     if (this.componentType.id) {
+      // Component type exists in the database: Update it.
       this.componentTypeService.updateType(this.componentType);
     } else {
-      this.componentTypeService.deleteType(this.componentType);
+      // Component type doesn't exists in the database: Create it.
+      this.componentTypeService.createType(this.componentType);
     }
 
     this.router.navigateByUrl('/componenttypes');
   }
 
 
+  /**
+   * Ask for the user's confirmation for deleting the current attribute.
+   *
+   * @author Nils Weber
+   */
   private confirmComponentTypeDeletion() {
     const dialogRef = this.dialog.open(ConfirmationDialogComponent);
     dialogRef.afterClosed().subscribe(confirmed => { if (confirmed) { this.deleteComponentType(); }});
   }
 
 
+  /**
+   * Delete the current attribute.
+   * This shouldn't be called directly from a button click since no confirmation is required.
+   *
+   * @author Nils Weber
+   */
   private deleteComponentType() {
     this.componentTypeService.deleteType(this.componentType);
     this.router.navigateByUrl('/componenttypes');
